90e8ccd6ba4ebff7395b9dbe7e34833c5a09cfd2
Joel Holdsworth <joel@airwebreathe.org.uk>
2017-03-05 21:04:21 -0700

    examples: Added random_hop.py example

---
+++ examples/random_hop.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import adf435x.interfaces
+from adf435x import calculate_regs, make_regs
+import random
+import time
+
+intf = adf435x.interfaces.FX2()
+
+while True:
+    freq = random.randint(50, 100)
+    INT, MOD, FRAC, output_divider, band_select_clock_divider = \
+            calculate_regs(freq=freq)
+    regs = make_regs(INT=INT, MOD=MOD, FRAC=FRAC,
+            output_divider=output_divider,
+            band_select_clock_divider=band_select_clock_divider)
+    intf.set_regs(regs[::-1])
+    print('%0.1fMHz' % freq)
+    time.sleep(0.1)

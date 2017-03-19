59dfb4bf8826967303424620e4ed6d0ebd682b2c
Joel Holdsworth <joel@airwebreathe.org.uk>
2017-03-19 15:21:33 -0600

    firmware/stm32: Initial USB slave support

--- firmware/stm32/stm32adf435xfw.c
+++ firmware/stm32/stm32adf435xfw.c
@@ -25,17 +25,102 @@
 #define PORT_LED GPIOC
 #define PIN_LED GPIO13
 
+const struct usb_device_descriptor dev = {
+	.bLength = USB_DT_DEVICE_SIZE,
+	.bDescriptorType = USB_DT_DEVICE,
+	.bcdUSB = 0x0200,
+	.bDeviceClass = 0,
+	.bDeviceSubClass = 0,
+	.bDeviceProtocol = 0,
+	.bMaxPacketSize0 = 64,
+	.idVendor = 0x0456,
+	.idProduct = 0xb40d,
+	.bcdDevice = 0x0000,
+	.iManufacturer = 1,
+	.iProduct = 2,
+	.iSerialNumber = 0,
+	.bNumConfigurations = 1,
+};
+
+const struct usb_interface_descriptor iface = {
+	.bLength = USB_DT_INTERFACE_SIZE,
+	.bDescriptorType = USB_DT_INTERFACE,
+	.bInterfaceNumber = 0,
+	.bAlternateSetting = 0,
+	.bNumEndpoints = 0,
+	.bInterfaceClass = 0xFF,
+	.bInterfaceSubClass = 0,
+	.bInterfaceProtocol = 0,
+	.iInterface = 0,
+};
+
+const struct usb_interface ifaces[] = {{
+	.num_altsetting = 1,
+	.altsetting = &iface,
+}};
+
+const struct usb_config_descriptor config = {
+	.bLength = USB_DT_CONFIGURATION_SIZE,
+	.bDescriptorType = USB_DT_CONFIGURATION,
+	.wTotalLength = 0,
+	.bNumInterfaces = 1,
+	.bConfigurationValue = 1,
+	.iConfiguration = 0,
+	.bmAttributes = 0x80,
+	.bMaxPower = 0x32,
+
+	.interface = ifaces,
+};
+
+const char *usb_strings[] = {
+	"ANALOG DEVICES",
+	"ADF4xxx USB Eval Board"
+};
+
+uint16_t usbd_control_buffer[64];
+
+static int vendor_control_callback(usbd_device *usbd_dev, struct usb_setup_data *req, uint8_t **buf,
+		uint16_t *len, void (**complete)(usbd_device *usbd_dev, struct usb_setup_data *req))
+{
+	(void)buf;
+	(void)len;
+	(void)complete;
+	(void)usbd_dev;
+
+	if (req->bmRequestType != 0x40)
+		return USBD_REQ_NOTSUPP;
+
+	gpio_toggle(PORT_LED, PIN_LED);
+
+	return USBD_REQ_HANDLED;
+}
+
+static void usb_set_config_cb(usbd_device *usbd_dev, uint16_t wValue)
+{
+	(void)wValue;
+	usbd_register_control_callback(usbd_dev, USB_REQ_TYPE_VENDOR,
+		USB_REQ_TYPE_TYPE, vendor_control_callback);
+}
+
 int main(void)
 {
+	usbd_device *usbd_dev;
+
 	rcc_clock_setup_in_hse_8mhz_out_72mhz();
 
 	rcc_periph_clock_enable(RCC_GPIOA);
 	rcc_periph_clock_enable(RCC_GPIOC);
+	rcc_periph_clock_enable(RCC_OTGFS);
 
 	/* LED output */
 	gpio_set_mode(PORT_LED, GPIO_MODE_OUTPUT_2_MHZ,
 		GPIO_CNF_OUTPUT_PUSHPULL, PIN_LED);
 
-	while (1);
+	usbd_dev = usbd_init(&st_usbfs_v1_usb_driver, &dev, &config, usb_strings, 2,
+		(uint8_t*)usbd_control_buffer, sizeof(usbd_control_buffer));
+	usbd_register_set_config_callback(usbd_dev, usb_set_config_cb);
+
+	while (1)
+		usbd_poll(usbd_dev);
 }
 
